611b83a2cdc45f96b5640b87872789183b4c231a
intruder5 <rickgucci9@gmail.com>
2026-03-13 19:27:31 -0500

    analytics upd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import Nav from "@/components/Hero/nav";
 import Crd from "@/components/Hero/card";
 import Third from "@/components/Hero/third";
 import Footer from "@/components/Hero/footer";
-
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
     return (
         <div>
+            <Analytics />
             <Nav/>
             <Hero/>
             <Crd />
